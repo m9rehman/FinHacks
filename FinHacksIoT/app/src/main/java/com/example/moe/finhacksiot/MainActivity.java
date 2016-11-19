9cdfb0fb99ff43eb3c21682b92efc03057b3346c
Muhammad Rehman <thatsocliche@hotmail.com>
2016-11-19 14:28:15 -0500

    Linked button to start Notification Activity

--- FinHacksIoT/app/src/main/java/com/example/moe/finhacksiot/MainActivity.java
+++ FinHacksIoT/app/src/main/java/com/example/moe/finhacksiot/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.moe.finhacksiot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         mFinanceBtn = (Button) findViewById(R.id.graphBtn);
         mLocationBtn = (Button) findViewById(R.id.locationBtn);
         mAnalysisBtn = (Button) findViewById(R.id.analysisBtn);
+
+        mLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotificationActivity.class);
+                startActivity(i);
+            }
+        });
 
 
     }
--- FinHacksIoT/app/src/main/java/com/example/moe/finhacksiot/NotificationActivity.java
+++ FinHacksIoT/app/src/main/java/com/example/moe/finhacksiot/NotificationActivity.java
@@ -2,8 +2,14 @@ package com.example.moe.finhacksiot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+import java.util.UUID;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -17,6 +23,31 @@ public class NotificationActivity extends AppCompatActivity {
 
         mSendBtn = (Button) findViewById(R.id.sendNotifBtn);
         mMessageView = (EditText) findViewById(R.id.pebbleMessage);
-        
+
+        // Create a new dictionary
+        final PebbleDictionary dict = new PebbleDictionary();
+
+        // The key representing a contact name is being transmitted
+        final int AppKeyContactName = 0;
+        final int AppKeyAge = 1;
+
+        // Get data from the app
+        final String contactName = "Moe";
+        final int age = 19;
+
+        // Add data to the dictionary
+        dict.addString(AppKeyContactName, contactName);
+        dict.addInt32(AppKeyAge, age);
+
+        final UUID appUuid = UUID.fromString("EC7EE5C6-8DDF-4089-AA84-C3396A11CC95");
+
+        mSendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Send the dictionary
+                PebbleKit.sendDataToPebble(getApplicationContext(), appUuid, dict);
+            }
+        });
+
     }
 }
