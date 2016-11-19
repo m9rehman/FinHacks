0fa22d329512b0722a1972646c7cbd13c519e0e2
Muhammad Rehman <thatsocliche@hotmail.com>
2016-11-19 12:48:31 -0500

    Plotted Juliens Losses over the years

--- mychatbotapp/analysis.py
+++ mychatbotapp/analysis.py
@@ -1,10 +1,29 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
 
-fInfo = pd.read_csv('banking.csv')
+# Converter function
+datefunc = lambda x: mdates.date2num(datetime.strptime(x, '%Y-%m-%d'))
+
+fInfo = pd.read_csv('banking2.csv')
+
+
+fInfo["Timestamp"] = fInfo["Date"].apply(datefunc)
+# print(fInfo["Timestamp"][530:])
+
+
 
 # Julian's starting Balance as of July 1st
-startingBalance = 6500
+
+
+# Total Spent
+fInfo = fInfo.sort('Timestamp')
+print(fInfo)
+
+
+startingBalance = 60000
 def costSubtract(x):
 	global startingBalance
 	amount = startingBalance - x
@@ -14,12 +33,21 @@ def costSubtract(x):
 # Creating a remaining Balance series	
 fInfo["AmountRemaining"] = fInfo["Spent"].apply(costSubtract)
 
-# Total Spent
-# print(fInfo["Spent"].sum())
 # print(fInfo["AmountRemaining"])
 
-print(pd.value_counts(fInfo["Location"]))
+# print(pd.value_counts(fInfo["Location"]))
+
+timeList = fInfo["Timestamp"].tolist()
+balanceList = fInfo["AmountRemaining"].tolist()
 
 
-# fInfo.to_csv('FinalReport.csv')
+
+# print(timeList)
+
+
+plt.plot(timeList,balanceList)
+plt.show()
+
+
+fInfo.to_csv('FinalReport3.csv')
 
